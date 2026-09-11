7b77476d3a83c64a0eed3ed53b4448ea3b46b155
almirahmad <ranaa810@gmail.com>
2026-09-11 19:13:57 +0500

    Restore Special Love: Nano Urdu + little peeps names

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -39,8 +39,9 @@ export const EVENT = {
     "Mr. & Mrs. Safdar Hussain",
     "Mr. & Mrs. Sheikh Zeeshan",
   ],
-  specialLove: "Almir & Hanaa",
-  belovedNano: "Mrs. Khurshid Bano",
+  specialLove: "Almir, Minha, Muhammad, Mahad, Rohan & Hanaa",
+  specialLoveLabel: "Our little peeps",
+  belovedNano: "محترمہ خورشید بانو",
   contacts: [
     { name: "Haji Makhan", phone: "0306-1614928", tel: "+923061614928" },
     { name: "Rana Ahmad", phone: "0304-2463055", tel: "+923042463055" },
